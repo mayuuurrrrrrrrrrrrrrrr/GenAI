9383a8d32a00a07845cb3fcb72dac2ca1e088e2c
Mayur Buddhe <23071355@ycce.in>
2026-02-21 15:58:25 +0530

    this is my first project on languge

---
+++ langchain.py
@@ -0,0 +1,25 @@
+from google import genai
+import streamlit as st
+
+API_KEY = st.secrets["GOOGLE"]["api_key"]
+client = genai.Client(api_key=API_KEY)
+
+def ask_math(question: str):
+    prompt = f"""
+You are a calculator that responds with math only.
+
+Answer this math question: what is two plus two?
+2+2=4
+
+Answer this math question: {question}?
+"""
+    
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents=prompt
+    )
+    
+    return response.text
+
+result = ask_math("what is square root of 25")
+st.write(result)
